273d6218dc1c430a878fab61080b303df64c614d
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 16:05:38 -0400

    created Home boilerplate && imported in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home'
 
 class App extends React.Component {
     render() {
         return (
         <div className="App">
-            App Component    
+            <Home />
         </div>
         );
     }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+    <div>
+        <h1>Courtside.</h1>
+        <p>View Baskeball Courts From All Over the World</p>
+    </div>
+    )
+}
+
+export default Home;
