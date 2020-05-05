eb513b8443f815f0d543ce8828bd999e203db462
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 18:27:51 -0400

    imported react-router-dom macros && routed to Courts component

--- src/containers/CourtsContainer.js
+++ src/containers/CourtsContainer.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Courts from '../components/Courts'
+import { Route, Switch } from 'react-router-dom';
 
 class CourtsContainer extends React.Component {
 
     render () {
         return (
         <div>
-            Courts Container
+            <Switch>
+                <Route exact path="/courts" component={Courts}/>
+            </Switch>
         </div>
         )
     }
