4ed909f2e13e5ed05b75d09347c49587401e0ceb
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 17:13:31 -0400

    imported Navbar in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home'
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
     render() {
