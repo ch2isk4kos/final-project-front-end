ecd334c6438d7527eb92046da6e1ee0ab3df390c
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 17:39:24 -0400

    created directory && Courts boilerplate

---
+++ src/containers/Courts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Courts extends React.Component {
+
+    render () {
+        return (
+        <div>
+            Courts Container
+        </div>
+        )
+    }
+}
+
+export default Courts;
