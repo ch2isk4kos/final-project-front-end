77d05f1136820d52ff97eae7869ea58d4d901524
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 15:59:50 -0400

    refactored App to a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+        <div className="App">
+            App Component    
+        </div>
+        );
+    }
 }
 
 export default App;
