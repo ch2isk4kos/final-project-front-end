e18d9ccf7fd2364d5dcc180225ec99c4a2af2691
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 20:10:42 -0400

    mapped over props && rendered

--- src/components/Courts.js
+++ src/components/Courts.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Courts = props => {
 
+    const courts = props.courts.length > 0 ?
+        props.courts.map(court => (<h4 key={court.id}>{court.name}</h4>)) :
+        null
+
     return (
     <div>
         <h1>Courts</h1>
+        { courts }
     </div>
     )
 }
