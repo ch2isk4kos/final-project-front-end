88abfab8a55964240d24f9da8a975663af5aa36c
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 21:41:04 -0400

    added action creator for new court form submission

---
+++ src/redux/actions/addCourt.js
@@ -0,0 +1,17 @@
+export const addCourt = (state) => {
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/courts', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json
+            },
+            body: JSON.stringify(state)
+        })
+        .then(response => response.json())
+        .then(court => dispatch({
+            type: 'ADD_COURT',
+            court
+        }))
+    }
+}
