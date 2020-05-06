e0766285c99828cc9b28c83ad99c44547264acda
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 20:24:02 -0400

    linked each instance of a court to its show page

--- src/components/Courts.js
+++ src/components/Courts.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Courts = props => {
 
     const courts = props.courts.length > 0 ?
-        props.courts.map(court => (<h4 key={court.id}>{court.name}</h4>)) :
+        props.courts.map(court => (<h4 key={court.id}><Link to={`/courts/${court.id}`}>{court.name}</Link></h4>)) :
         null
 
     return (
