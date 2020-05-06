3a170c5d4246a8770095bc716163062ab6a5df19
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-05 20:29:35 -0400

    filtered over props for instance of court && rendered data

--- src/components/Court.js
+++ src/components/Court.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Court = props => {
 
+    const court = props.courts.length > 0 ?
+        props.courts.filter(c => c.id == props.match.params.id)[0] :
+        null
+
     return (
     <div>
-        <h1>Court</h1>
+        <h3>{court && court.name}</h3>
+        <img src={court && court.img_url} width="500" height="300"></img>
+        <h5>{court && court.address}</h5>
     </div>
     )
 }
