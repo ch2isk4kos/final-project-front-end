8ba05898c958c25c2b57f86153cc4df30c775f61
ch2isk4kos <ch2isk4kos@gmail.com>
2020-05-13 16:19:40 -0400

    refactored Courts Component

--- src/components/Courts.js
+++ src/components/Courts.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Courts = props => {
+const Courts = (props) => {
+    console.log("props: ", props);
+    console.log("props.courts: ", props.courts);
 
     const courts = props.courts.length > 0 ?
-        props.courts.map(court => (<h4 key={court.id}><Link to={`/courts/${court.id}`}>{court.name}</Link></h4>)) :
+        props.courts.map(court => <h4 key={court.id}><Link to={`/courts/${court.id}`}>{court.name}</Link></h4>) :
         null
+
+    // const courts = props.courts.map(court => <h4 key={court.id}><Link to={`/courts/${court.id}`}>{court.name}</Link></h4>)
 
     return (
     <div>
@@ -13,6 +17,7 @@ const Courts = props => {
         { courts }
     </div>
     )
+
 }
 
 export default Courts;
